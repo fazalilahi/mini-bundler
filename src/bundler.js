e8664c96eb7ec5a6b93e8cee5a2b3dff3a313e18
Fazal Ilahi <fazalchemmengath@gmail.com>
2025-03-30 00:33:49 +0530

    add: queue dependencies, wrapper functions and monolith code output(bundled), custom require and export, write file to dist

--- src/bundler.js
+++ src/bundler.js
@@ -10,7 +10,7 @@ const { transformFromAstSync } = require('@babel/core')
 const { default: traverse } = require('@babel/traverse')
 
 let ID = 0
-function composeFile(filePath) {
+function transformFile(filePath) {
     try {
 
         const file = fs.readFileSync(filePath, 'utf8');
@@ -42,7 +42,79 @@ function composeFile(filePath) {
             code,
         }
     } catch (error) {
-        console.error('Failed to compose file: ', error)
+        console.error('Failed to transform file: ', error)
     }
 }
-console.log(composeFile('./example/index.js'));
+
+async function createGraph (entry) {
+    const mainEntry = await transformFile(entry);
+    const queue = [mainEntry];
+    for (const asset of queue) {
+        asset.mapping = {};
+        const dirname = path.dirname(asset.filePath);
+
+        await Promise.all(
+            asset.dependencies.map(async relativePath => {
+                try {
+                    const absolutePath = path.join(dirname, relativePath);
+                    const child = await transformFile(absolutePath);
+                    asset.mapping[relativePath] = child.id;
+                    queue.push(child);
+                } catch (error) {
+                    console.error(`Failed to process ${relativePath}: `, error);
+                }
+            })
+        )
+    }
+    return queue;
+}
+
+function bundle(graph) {
+    const modules = graph.map(module => {
+        return `
+        ${module.id}: [
+        function(exports, module, require) {
+            ${module.code}
+        },
+        ${JSON.stringify(module.mapping)},
+        ]
+        `;
+    }).join(',');
+
+    return `
+    (function(modules) {
+        function require(id) {
+            const [fn, mapping] = modules[id];
+
+            function localRequire(name) {
+                const resolved = mapping[name];
+                if (resolved === undefined) {
+                throw new Error('Cannot find module: '+ name);
+                }
+                return require(resolved);
+            }
+            const module = { exports: {} };
+            fn(module.exports, module, localRequire);
+            return module.exports;
+        }
+        require(0);
+    })({${modules}})
+    `;
+}
+
+async function build() {
+    try {
+        const graph = await createGraph('./example/index.js');
+        const result = bundle(graph);
+
+        await fs.promises.mkdir('dist', { recursive: true });
+        await fs.promises.writeFile('dist/bundle.js', result);    
+        // console.log(result)
+        console.log('Bundle successfully created!');
+        console.log('Output: dist/bundle.js (' + (result.length/1024).toFixed(2) + 'KB)');
+    } catch (error) {
+        console.error('Failed to create bundle: ', error)
+    }
+}
+build();
+
